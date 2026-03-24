9f6147738bcc1ff42694d4d1258b31a16261497f
Jinny Shah <osianmata1983@gmail.com>
2026-03-24 19:39:04 +0000

    Revert to ExpressJS deployment and remove Next/Netlify files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  },
-};
-
-module.exports = nextConfig;
