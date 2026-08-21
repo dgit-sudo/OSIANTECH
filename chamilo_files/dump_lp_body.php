c2e874caaff177664ff7c96f5687daf55db66125
dgit-sudo <dgit-sudo@users.noreply.github.com>
2026-08-21 11:07:06 +0530

    Chamilo Diagnostics: Add dump_lp_body.php

---
+++ chamilo_files/dump_lp_body.php
@@ -0,0 +1,11 @@
+<?php
+require_once '/var/www/html/chamilo/vendor/autoload.php';
+
+$ch = curl_init('http://localhost/main/lp/lp_controller.php?action=view&lp_id=1&cidReq=ORCLADMIN1');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+$html = curl_exec($ch);
+curl_close($ch);
+
+$bodyStart = strpos($html, '<body');
+echo substr($html, $bodyStart, 3000) . "\n";
