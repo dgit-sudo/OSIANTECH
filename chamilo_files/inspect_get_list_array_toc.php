ae813e3347fb48f25e27810d9ebfe7163fb22a81
dgit-sudo <dgit-sudo@users.noreply.github.com>
2026-08-21 11:25:36 +0530

    Chamilo Diagnostics: Update inspect_get_list_array_toc.php

--- chamilo_files/inspect_get_list_array_toc.php
+++ chamilo_files/inspect_get_list_array_toc.php
@@ -1,4 +1,8 @@
 <?php
+require_once '/var/www/html/chamilo/vendor/autoload.php';
+require_once '/var/www/html/chamilo/public/main/inc/global.inc.php';
+require_once '/var/www/html/chamilo/public/main/lp/learnpath.class.php';
+
 echo "=== getListArrayToc() in learnpath.class.php ===\n";
 $ref = new ReflectionMethod('learnpath', 'getListArrayToc');
 echo "File: " . $ref->getFileName() . ":" . $ref->getStartLine() . "\n";
