697fb612f13a954e8058479a18dc0a11e8c84d34
dgit-sudo <dgit-sudo@users.noreply.github.com>
2026-08-21 10:44:37 +0530

    Chamilo Diagnostics: Add test_lp_get_link.php

---
+++ chamilo_files/test_lp_get_link.php
@@ -0,0 +1,18 @@
+<?php
+require_once '/var/www/html/chamilo/public/main/inc/global.inc.php';
+require_once '/var/www/html/chamilo/public/main/lp/learnpath.class.php';
+require_once '/var/www/html/chamilo/public/main/lp/learnpathItem.class.php';
+
+$course = api_get_course_entity(6);
+$lpRepo = \Chamilo\CoreBundle\Framework\Container::getLpRepository();
+$lp = $lpRepo->find(249);
+
+echo "LP found: " . ($lp ? $lp->getTitle() : "NULL") . "\n";
+
+$oLP = new learnpath('ORCLADMIN1', 249, 1);
+echo "TOC:\n";
+print_r($oLP->get_toc());
+
+echo "\nLink for item 1:\n";
+$link = $oLP->get_link('http', 1);
+echo "get_link: $link\n";
